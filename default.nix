22a389d314434a272d8b45e76b34c483b1a9f5c0
hxpe-dev <316854@vassaux.net>
2025-10-15 18:50:57 +0200

    Bump to v0.2.18

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "kotofetch";
-  version = "0.2.17";
+  version = "0.2.18";
 
   src = pkgs.fetchurl {
     url = "https://github.com/hxpe-dev/kotofetch/releases/download/v${version}/kotofetch-v${version}-x86_64-unknown-linux-gnu.tar.gz";
