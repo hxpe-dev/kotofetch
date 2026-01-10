b7195f6f793e2ebb70247fb3dbefb4ecbe788cbc
hxpe <107758731+hxpe-dev@users.noreply.github.com>
2026-01-10 12:18:07 +0100

    Fix NixOS install failure
    Fixes #19

--- default.nix
+++ default.nix
@@ -1,24 +1,24 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.stdenv.mkDerivation rec {
+pkgs.rustPlatform.buildRustPackage rec {
   pname = "kotofetch";
   version = "0.2.18";
 
-  src = pkgs.fetchurl {
-    url = "https://github.com/hxpe-dev/kotofetch/releases/download/v${version}/kotofetch-v${version}-x86_64-unknown-linux-gnu.tar.gz";
-    sha256 = "sha256-U0Bwm++/WTU0Lkv1lC518JCHwBIBno/SqSQQiGfbOJk="; # fill in
+  src = pkgs.fetchFromGitHub {
+    owner = "hxpe-dev";
+    repo = "kotofetch";
+    rev = "v${version}";
+    sha256 = "sha256-sU+GeZKr8Tpg52XzxLmuA3NyqA47wqANL4yReFJpI4M=";
   };
 
-  installPhase = ''
-    mkdir -p $out/bin
-    tar -xzf $src --strip-components=1 -C $out/bin
-  '';
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
 
   meta = with pkgs.lib; {
     description = "Minimalist fetch tool for Japanese quotes (written in Rust)";
     homepage = "https://github.com/hxpe-dev/kotofetch";
     license = licenses.mit;
-    maintainers = [ ];
     platforms = platforms.unix;
   };
 }
