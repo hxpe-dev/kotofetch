11f617837158ce9a31e7e93f54ff690307049471
hxpe <316854@vassaux.net>
2026-05-06 00:44:03 +0200

    chore: nix bump to v0.2.22

--- default.nix
+++ default.nix
@@ -2,16 +2,16 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "kotofetch";
-  version = "0.2.21";
+  version = "0.2.22";
 
   src = pkgs.fetchFromGitHub {
     owner = "hxpe-dev";
     repo = "kotofetch";
     rev = "v${version}";
-    sha256 = "sha256-mll98MC/kB3BisC9teohlBDW6jgaOwMJRqsoGLjsSpA=";
+    sha256 = "sha256-aY8HRKSHLQKjl4b7v5q3SeNMc+GJPnE2XVrEsl+nGR0=";
   };
 
-  cargoHash = "sha256-nqTEAe7ODBg5SFxVdWW9AckT/y2YKZTAmlBtGqZ0ysE=";
+  cargoHash = "sha256-r36x/I/RaIWFEoDYXf3edpLeqGvEyozhT4EuCTSEe/k=";
 
   meta = with pkgs.lib; {
     description = "Minimalist fetch tool for Japanese quotes (written in Rust)";
