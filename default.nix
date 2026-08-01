6da1be7dcecfa8503e1037f87e195f95d85956d8
hxpe-dev <316854@vassaux.net>
2026-08-01 10:23:46 +0000

    chore: nix bump to v0.2.23

--- default.nix
+++ default.nix
@@ -2,16 +2,16 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "kotofetch";
-  version = "0.2.22";
+  version = "0.2.23";
 
   src = pkgs.fetchFromGitHub {
     owner = "hxpe-dev";
     repo = "kotofetch";
     rev = "v${version}";
-    sha256 = "sha256-aY8HRKSHLQKjl4b7v5q3SeNMc+GJPnE2XVrEsl+nGR0=";
+    sha256 = "sha256-mW0oMrmDUn8qsCCu870z6zS3szGVyCU4wnSytH6DkUE=";
   };
 
-  cargoHash = "sha256-r36x/I/RaIWFEoDYXf3edpLeqGvEyozhT4EuCTSEe/k=";
+  cargoHash = "sha256-g5SLS6PpcRNm1zcHkX8pvqk7s2yQ+zUAjp4CSCq/U/o=";
 
   meta = with pkgs.lib; {
     description = "Minimalist fetch tool for Japanese quotes (written in Rust)";
